e9f6817fbe37a8b2932ab867a83659d1fd49a269
Yennhi Dang <nhidangn@gmail.com>
2023-04-17 15:04:56 -0700

    first commit

---
+++ SongService.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public interface SongService {
+    Song searchById(Integer songID);
+    List<Song> searchByTitle(String title);
+    List<Song> searchByAlbum(String album);
+}
