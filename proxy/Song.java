735c11a94ea2a92c3e75594e3f4c02e922304580
Yennhi Dang <nhidangn@gmail.com>
2023-04-17 15:18:16 -0700

    declare variables

---
+++ proxy/Song.java
@@ -0,0 +1,10 @@
+package proxy;
+
+public class Song {
+    
+    String title;
+    String artist;
+    String album;
+    int duration;
+    
+}
--- proxy/SongService.java
+++ proxy/SongService.java
@@ -1,3 +1,4 @@
+package proxy;
 import java.util.List;
 
 public interface SongService {
